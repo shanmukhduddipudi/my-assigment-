229d0b76e3b76dd746f416a30cec2e182f1ab0e3
shanmukhduddipudi <124775521+shanmukhduddipudi@users.noreply.github.com>
2023-02-08 16:33:10 +0530

    Add files via upload

---
+++ subtractions.sol
@@ -0,0 +1,11 @@
+//SPDX -License - Identifier:MIT 
+pragma solidity ^0.8.0;
+contract sub{
+function subtraction(uint _a ,uint _b) public pure returns(uint){
+  uint value= _a - _b;
+  return value;
+
+}
+
+
+}
