90cb16fa1bf2bd6e0300b4ce1274950527ee9284
shanmukhduddipudi <124775521+shanmukhduddipudi@users.noreply.github.com>
2023-02-08 16:31:50 +0530

    Add files via upload

---
+++ division (1).sol
@@ -0,0 +1,11 @@
+//SPDX -License - Identifier:MIT 
+pragma solidity ^0.8.0;
+
+contract div{
+  
+   function divison(uint _a ,uint _b) public pure returns(uint){
+       uint total= _a / _b;
+       return total;
+
+}
+}
