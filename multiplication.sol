8820186f61f439c53e295ffb1feb4114a9e86292
shanmukhduddipudi <124775521+shanmukhduddipudi@users.noreply.github.com>
2023-02-08 16:34:31 +0530

    Add files via upload

---
+++ multiplication.sol
@@ -0,0 +1,10 @@
+//SPDX -License - Identifier:MIT 
+pragma solidity ^0.8.0;
+contract mult{
+
+function multiplay(uint _num1 ,uint _num2) public pure returns(uint){
+  uint totalvalue= _num1 * _num2;
+  return totalvalue;
+
+}
+}
